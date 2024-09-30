7ead3afb77565ab0e0fb401a04896327062942ef
fabufarha11 <fabufar1@emich.edu>
2024-09-30 14:05:04 -0400

    Sad file created and invoked

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,12 @@
 
-public class Driver {
+public class Driver extends Sad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Our team is getting started.");
+		Driver r = new Driver();
+		r.sad();
+		
 	}
 
 }
